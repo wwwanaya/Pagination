db8b2a459f2ca40ad41d48c527f843a68cb30df8
Kevin Anaya <www.anaya@gmail.com>
2015-03-03 11:39:00 -0600

    First upload of src...

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+	<!-- Standard Meta -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<!-- /Standard Meta -->
+	<title>Pagination...</title>
+	<link rel="stylesheet" type="text/css" href="Se/dist/semantic.min.css">
+  <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+  <script type="text/javascript" src="Se/dist/semantic.min.js"></script>
+</head>
+<body>
+  <div class="ui grid">
+    <div class="two wide column"></div>
+    <div class="twelve wide column">
+      <table class="ui table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Company Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>County</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Telephone 1</th>
+            <th>Telephone 2</th>
+            <th>E-Mail</th>
+            <th>Webpage</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  </div>
+  <div class="two wide column"></div>
+</body>
+</html>
